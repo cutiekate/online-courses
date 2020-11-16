3cbe08c80a205b75cf9dbc1b7aa9a5787451f598
cutiekate <cutiekate@outlook.com>
2020-11-16 10:02:42 +0200

    almost done

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
-new WOW().init();
+
+
+
+$( document ).ready(function() {
+    $('.owl-carousel').owlCarousel({
+        items: 1,
+        autoplay: true,
+        autoWidth: true,
+        responsive:{
+            320:{
+                items:1,
+                autoWidth: false,
+            },
+            375:{
+                items:1,
+                autoWidth: false,
+            },
+            414:{
+                items:1,
+                autoWidth: false,
+            },
+            768:{
+                items:1,
+            }
+        }
+    })
+    
+});
